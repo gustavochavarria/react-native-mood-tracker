0f5d6793bae6efa5f840516c70acabf6987dc873
gchava92 <gchava92@gmail.com>
2022-04-11 12:02:16 -0600

    Config ts and styled-components

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import styled from 'styled-components/native'
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <Container>
+      <Text>Hola mundo!</Text>
       <StatusBar style="auto" />
-    </View>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Container = styled.View`
+flex: 1;
+background-color: #fff;
+align-items: center;
+justify-content: center;
+`;
