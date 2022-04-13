e8ac06e1e463ebed7619c578ee9554dd380cb80b
gchava92 <gchava92@gmail.com>
2022-04-13 14:35:04 -0600

    Add splash & expo icons

--- src/navigations/BottomTabs.navigator.tsx
+++ src/navigations/BottomTabs.navigator.tsx
@@ -1,15 +1,33 @@
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {Home} from '../screens/Home.screen'
-import {History} from '../screens/History.screen'
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import { FontAwesome5 } from "@expo/vector-icons";
+
+import { Home } from "../screens/Home.screen";
+import { History } from "../screens/History.screen";
 
 const BottomTabs = createBottomTabNavigator();
 
 export const BottomTabsNavigator: React.FC = () => {
+  return (
+    <BottomTabs.Navigator
+      screenOptions={({ route }) => {
+        return {
+          tabBarIcon: ({ color, size }) => {
+            if (route.name === "Home") {
+              return (
+                <FontAwesome5 name="house-user" size={size} color="gray" />
+              );
+            }
 
-    return (
-        <BottomTabs.Navigator>
-            <BottomTabs.Screen name="Home" component={Home} />
-            <BottomTabs.Screen name="History" component={History} />
-        </BottomTabs.Navigator>
-    );
-}
+            if (route.name === "History") {
+              return <FontAwesome5 name="history" size={size} color="gray" />;
+            }
+          },
+        };
+      }}
+    >
+      <BottomTabs.Screen name="Home" component={Home} />
+      <BottomTabs.Screen name="History" component={History} />
+    </BottomTabs.Navigator>
+  );
+};
